2b9cdc3baad48939cedd41e5531079beac2b31ac
Lance Dang <lancedang64@gmail.com>
2021-05-24 17:19:39 +1000

    Added logic to toggle between C and F. Modified UI

--- src/index.js
+++ src/index.js
@@ -27,5 +27,27 @@ function addSearchListener() {
   });
 }
 
+function addUnitsListener() {
+  const units = document.querySelectorAll('input[name=units]');
+  units.forEach((unit) => unit.addEventListener('change', changeUnits));
+}
+
+function changeUnits() {
+  const currentUnits = getUnitsFromInput();
+  const tempNums = document.querySelectorAll('.tempNum');
+  tempNums.forEach((tempNum) => convertUnit(tempNum, currentUnits));
+}
+
+function convertUnit(tempNum, currentUnits) {
+  if (currentUnits === 'imperial') {
+    const cToF = (parseFloat(tempNum.textContent) * 9) / 5 + 32;
+    tempNum.textContent = parseFloat(cToF.toFixed(2));
+  } else {
+    const fToC = ((parseFloat(tempNum.textContent) - 32) * 5) / 9;
+    tempNum.textContent = parseFloat(fToC.toFixed(2));
+  }
+}
+
 renderWeatherData();
 addSearchListener();
+addUnitsListener();
